13c8dc7766da88f6bf547e0a5d9874513a0a741b
Al-Giovinetti <giovinettialessio@gmail.com>
2023-05-29 15:33:04 +0200

    Creata arrey con oggetti

---
+++ js/myscript.js
@@ -0,0 +1,40 @@
+const ourTeam = [
+    subject = {
+        name: "Wayne",
+        surname: "Barnet",
+        role: "ounder & CEO",
+        img: "wayne-barnett-founder-ceo.jpg"
+    },
+    subject = {
+        name: "Angela",
+        surname: "Caroll",
+        role: "Chief Editor",
+        img: "angela-caroll-chief-editor.jpg"
+    },
+    subject = {
+        name: "Walter",
+        surname: "Gordon",
+        role: "Office Manager",
+        img: "walter-gordon-office-manager.jpg"
+    },
+    subject = {
+        name: "Angela",
+        surname: "Lopez",
+        role: "Social Media Manager",
+        img: "angela-lopez-social-media-manager.jpg"
+    },
+    subject = {
+        name: "Scott",
+        surname: "Estrada",
+        role: "Developer",
+        img: "scott-estrada-developer.jpg"
+    },
+    subject = {
+        name: "Barbara",
+        surname: "Ramos",
+        role: "Graphic Designer",
+        img: " barbara-ramos-graphic-designer.jpg"
+    }
+]
+
+console.log(ourTeam)
