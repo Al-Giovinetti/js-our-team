1cc6bf4733172c46838e0e214433b635766cdd91
Al-Giovinetti <giovinettialessio@gmail.com>
2023-05-29 17:33:26 +0200

    Sistemato elenco in console

--- js/myscript.js
+++ js/myscript.js
@@ -1,35 +1,35 @@
 const ourTeam = [
-    subject = {
+    {
         name: "Wayne",
         surname: "Barnet",
         role: "ounder & CEO",
         img: "wayne-barnett-founder-ceo.jpg"
     },
-    subject = {
+    {
         name: "Angela",
         surname: "Caroll",
         role: "Chief Editor",
         img: "angela-caroll-chief-editor.jpg"
     },
-    subject = {
+    {
         name: "Walter",
         surname: "Gordon",
         role: "Office Manager",
         img: "walter-gordon-office-manager.jpg"
     },
-    subject = {
+    {
         name: "Angela",
         surname: "Lopez",
         role: "Social Media Manager",
         img: "angela-lopez-social-media-manager.jpg"
     },
-    subject = {
+    {
         name: "Scott",
         surname: "Estrada",
         role: "Developer",
         img: "scott-estrada-developer.jpg"
     },
-    subject = {
+    {
         name: "Barbara",
         surname: "Ramos",
         role: "Graphic Designer",
@@ -37,12 +37,10 @@ const ourTeam = [
     }
 ]
 
-const ulElement = document.querySelector("ul")
+const ulElement = document.querySelector("ul");
 
 for(let i=0 ; i < ourTeam.length ; i++){
-    let liElement = document.createElement("li")
-    liElement.innerHTML = JSON.stringify(ourTeam[i])
-    ulElement.append(liElement)   
-    console.log(ourTeam[i])
+    const subject = ourTeam[i];
+    console.log(subject.name,subject.surname,subject.role,subject.img)  
 }
 
